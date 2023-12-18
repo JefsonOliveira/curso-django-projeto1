d95a4ea9c33f0d1f5e74650d2dcd801fefb1f275
Jefson Oliveira <kaironoliveira16@gmail.com>
2023-12-18 19:15:42 -0400

    feat: Adição de Urls

--- recipes/views.py
+++ recipes/views.py
@@ -2,5 +2,11 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Otávio',
+    })
 
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Otávio',
+    })
